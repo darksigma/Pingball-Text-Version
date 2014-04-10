86e3a5cfd7cfe6258d9b706bc5579cd8a72b8139
Dana Reback <dreback@mit.edu>
2014-04-10 14:01:56 -0400

    fixed formatting

--- src/Board.java
+++ src/Board.java
@@ -1,30 +1,33 @@
-/*
- * Thread safety strategy:
- *-Synchronize everything!
- *-Make sure each board object only belongs to one board at a time
- *-Make everything immutable that can possibly be immutable
-*/
+    /*
+     * Thread safety strategy:
+     *-Synchronize everything!
+     *-Make sure each board object only belongs to one board at a time
+     *-Make everything immutable that can possibly be immutable
+    */
+
 //specs for Board class
-
-getXMax()
-/**
- *Returns the width of the board
- *@return integer representing the width of the board
-**/
-
-getYMax()
-/**
- *Returns the length of the board
- *@return integer representing the length of the board
-**/
-
-addToBoard()
-/**
- *adds an item to the board
- *@param item BoardItem to be added to the board
- **/
-
-step()
-/**
- *Updates the board after one timestep
-**/
+public abstract class Board{
+    
+    /**
+     *Returns the width of the board
+     *@return integer representing the width of the board
+    **/
+    public abstract int getXMax();
+    
+    /**
+     *Returns the length of the board
+     *@return integer representing the length of the board
+    **/
+    public abstract int getYMax();
+    
+    /**
+     *adds an item to the board
+     *@param item BoardItem to be added to the board
+     **/
+    public abstract void addToBoard();
+    
+    /**
+     *Updates the board after one timestep
+    **/
+    public abstract void step();
+}
--- src/BoardItem.java
+++ src/BoardItem.java
@@ -7,28 +7,31 @@
 
 //specs for boardItem interface
 
-getX()
-/**
- *Returns the x-coordinate of the center of the item
- *@return integer representing the x-coordinate
-**/
+public abstract interface BoardItem{
+    
+    /**
+     *Returns the x-coordinate of the center of the item
+     *@return integer representing the x-coordinate
+    **/
+    public abstract int getX();
 
-getY()
-/**
- *Returns the y-coordinate of the center of the item
- *@return integer representing the y-coordinate
-**/
+    /**
+     *Returns the y-coordinate of the center of the item
+     *@return integer representing the y-coordinate
+    **/
+    public abstract int getY();
 
-step()
-/**
- *Allows the item to move about the board. If the item encounters the wall of the board,
- *the item is reflected appropriately. If the item does not move, step returns the item's current
- *position
- *@param timeStep a double representing the time increment by which the velocity changes; cannot be null
-**/
+    /**
+     *Allows the item to move about the board. If the item encounters the wall of the board,
+     *the item is reflected appropriately. If the item does not move, step returns the item's current
+     *position
+     *@param timeStep a double representing the time increment by which the velocity changes; cannot be null
+    **/
+    public abstract void step();
 
-charRep()
-/**
- *return a character representation of the board item for printing
- *@return a character representation of the board item
-**/
+    /**
+     *return a character representation of the board item for printing
+     *@return a character representation of the board item
+    **/
+    public abstract void charRep();
+}
