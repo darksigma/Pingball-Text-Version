f8eff9c8c115f0a31ae80fbf62738ff590a392b1
Dana Reback <dreback@mit.edu>
2014-04-14 21:57:20 -0400

    updated client

--- src/pingball/client/PingballClient.java
+++ src/pingball/client/PingballClient.java
@@ -4,10 +4,20 @@ import boardPhysics.Board;
 
 public class PingballClient{
 	
+   //the client's board, read from the file
+    private final Board board;
+    
+    public PingballClient(Board board){
+        this.board = board;
+    }
+    
 	/**
+     * Usage: PingballServer FILE
+     * 
      * Clients are built to connect to a server and pass balls to the server,
      * they read a board via a parser from a file and can connect their boards 
      * to other boards by setting walls as invisible. 
+     * 
      */
     public static void main(String[] args) {
         //TODO
